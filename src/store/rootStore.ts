003760961cf15b9922a28d6a9216e7744964c510
Mikael MOREAU <mikaelmoreau@yahoo.fr>
2020-12-12 11:33:13 +0100

    added a logger middleware for redux

--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -1,8 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { counterReducer } from './counter/CounterReducer';
+import { createLogger } from 'redux-logger';
+
+const logger = createLogger();
 
 export const rootReducer = combineReducers({ counterReducer });
 
 export type AppState = ReturnType<typeof rootReducer>;
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(logger));
